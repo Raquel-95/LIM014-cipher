92d1be9672a41e5ab58027c9103f7fa5f71bd081
Raquel-95 <fannypastor195@gmail.com>
2021-02-03 01:18:29 -0500

    Updates

--- cipher.js
+++ cipher.js
@@ -1,41 +1,39 @@
 const cipher = {
-  encode: function(offset,input)  {
+  encode: function(offset,input)  { //Dentro del objeto cipher creo una función llamada encode, que tiene parametros llamadas offset e input.
     
+    //condicionales para hacer el testeo unitario. Para evitar que se produzcan errores
     if(offset === undefined || input === undefined) {
-      throw new TypeError("invoked with wrong argument types ...");
+      throw new TypeError("invoked with wrong argument types ..."); //retorna el error.
     }
     else if(offset == 0 && input == undefined){
       throw new TypeError("invoked with wrong argument types (0)");
     }
-
     else if(offset === null && Array.isArray(input)){
       throw new TypeError("invoked with wrong argument types (null, [])");
     }
-
     else if(offset == 0 && input == 0){
       throw new TypeError("invoked with wrong argument types 0, 0");
     }
 
-    let palabra_codificada= ''; // cree variable para guardar el resultado
+    let palabra_codificada= ''; // cree variable para guardar el resultado.
+
     //Cree un bucle con for, con los limites de inicio, final e incremento.
-    
     for (let i = 0; i < input.length; i++) {
      
-      const ascii_original = input.charCodeAt(i); 
-      if(ascii_original>=65 && ascii_original <=90 || ascii_original>=97 && ascii_original<=122){
+      const ascii_original = input.charCodeAt(i); //para obtener el código ascii
+      if(ascii_original>=65 && ascii_original <=90 || ascii_original>=97 && ascii_original<=122){//condición para no codificar caracteres especiales
         
         const ascii_desplazado = (ascii_original - 65 + offset) %26 + 65;
-        const letra_codificada = String.fromCharCode(ascii_desplazado);
+        const letra_codificada = String.fromCharCode(ascii_desplazado); //para convertir ascii a letra.
 
         palabra_codificada += letra_codificada; // para concatenar todas las letras.
       }
       else {
-        palabra_codificada += input.charAt(i);
+        palabra_codificada += input.charAt(i); // charAt método devuelve un carácter que está en una posición de un string.
       }
-      
+
     }
-  
-    return palabra_codificada; // return para retornar/devolver 
+    return palabra_codificada; // return para retornar/devolver el resultado. 
   },
 
   decode: function(offset,input)  {
@@ -77,8 +75,6 @@ const cipher = {
   }
   
 }
-
-//Dentro del objeto cipher creo una función llamada encode, que tiene parametros o variables llamadas offset e input.
 
 
 export default cipher;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import cipher from './cipher.js';
 
-const code = document.getElementById('btn_code'); //Para llamar al al boton code. 
-code.addEventListener('click', () => { // para agregar/asignar un evento. Escuchar.
+const encode = document.getElementById('btn_encode'); //Para llamar al boton code. 
+encode.addEventListener('click', () => { // para agregar/asignar un evento. Escuchar.
     const offset = document.getElementById('input_offset').value; // para obtener el offset, traer el valor y meterlo en una constante
     const input = document.getElementById('text_input').value; // 
     const output = document.getElementById('text_output'); // para acceder a la caja...
@@ -40,31 +40,5 @@ presentacion.addEventListener('mouseenter', () => {
 presentacion.addEventListener("mouseleave", () => {
     presentacion.innerHTML = "CJFOWFOJEP<br/><br/> BM TJTUFNB EF NFOTBKFSJB TFDSFUB <br/><br/>EF MBT GVFSABT BSNBEBT EFM QFSV"
 });
-//const copiar = document.getElementById('copiar'); 
-//copiar.addEventListener('click', () => {
-
-   // const output = document.getElementById('text_output');
-   // output.select();
-   // document.execCommand('copy');
-
-//});
-
-
-
-//function copiarAlPortapapeles(text_output) {
-   // var aux = document.createElement("output");
-  //  aux.setAttribute("value", document.getElementById(text_output).innerHTML);
-   // document.body.appendChild(aux);
-  //  aux.select();
-   // document.execCommand("copy");
- // document.body.removeChild(aux);
-  //}
-
-/*console.log(cipher);
-console.log(typeof cipher);
-console.log(typeof "Raquel");
-console.log(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'));
-console.log(cipher.decode(5, 'DEF'));
-console.log(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG'));*/
 
 
